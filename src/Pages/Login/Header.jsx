86bfb14e1c0870efc1a4fb1b4f520365998f3034
mijaplex <mijail.febres@gmail.com>
2021-09-13 17:46:39 +0200

    The header is now working

---
+++ src/Pages/Login/Header.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import styled from "styled-components";
+import MotionIcon from '../../Assets/images/logo.png'
+import PostsIcon from '../../Assets/images/posts_logo.png'
+import FindFriendsIcon from '../../Assets/svgs/icon-friends.svg'
+
+import NotificationIcon from '../../Assets/svgs/notification_bell.svg'
+import ProfileIcon from '../../Assets/images/users/jennifer.png'
+import MenuIcon from '../../Assets/svgs/menu.svg'
+
+const HeaderContainer = styled.div `
+    width: 100%;
+    height: 15%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    #left {
+        width: 50%;
+        height: 100%;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+
+        .DivButtons {
+            height: 100%;
+            width: 30%;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            .ButtonTop {
+                width: 30px;
+                height: 30px;
+                background-color: white;
+                border: none;
+                background-repeat: no-repeat;
+                background-position-x: center;
+                background-position-y: center;
+                background-size: contain;
+            }
+
+        }
+    }
+
+    #right {
+        width: 50%;
+        height: 100%;
+        display: flex;
+        justify-content: flex-end;
+
+        .ButtonBottom {
+            width: 30px;
+            height: 30px;
+            background-color: white;
+            border: none;
+            background-repeat: no-repeat;
+            background-position-x: center;
+            background-position-y: center;
+            background-size: contain;
+        }
+    }
+
+    #ButtonMotion {
+        /* width: ${props => props.iconSize}; */
+        background-image: url(${MotionIcon});
+    }
+    #ButtonPosts {
+        background-image: url(${PostsIcon});
+    }
+    #ButtonFindFriends {
+        background-image: url(${FindFriendsIcon});
+    }
+
+    #ButtonNotification {
+        background-image: url(${NotificationIcon});
+    }
+    #ButtonProfile {
+        background-image: url(${ProfileIcon});
+    }
+    #ButtonMenu {
+        background-image: url(${MenuIcon});
+    }
+`
+const MasterHeader = () => {
+
+    const handleOnClickMotion = () => {
+
+    }
+    const handleOnClickPosts = () => {
+
+    }
+    const handleOnClickFindFriends = () => {
+
+    }
+    const handleOnClickNotification = () => {
+
+    }
+    const handleOnClickNumberNotifications = () => {
+
+    }
+    const handleOnClickProfile = () => {
+
+    }
+    const handleOnClickMenu = () => {
+
+    }
+
+    return (
+        <HeaderContainer>
+            <div id='left'>
+                <div className = 'DivButtons' id='Motion' >
+                    <button onClick={handleOnClickMotion}className='ButtonTop' id='ButtonMotion'></button>
+                    <label>Motion</label>
+                </div>
+                <div className = 'DivButtons' id='Posts' >
+                    <button onClick={handleOnClickPosts}className='ButtonTop' id='ButtonPosts'></button>
+                    <label>Posts</label>
+                </div>
+                <div className = 'DivButtons' id='FindFriends'>
+                    <button onClick ={handleOnClickFindFriends} className='ButtonTop' id='ButtonFindFriends'></button>
+                    <label>Find Friends</label>
+                </div>
+            </div>
+            <div id='right'>
+                <button onClick={handleOnClickNotification} className='ButtonBottom' id='ButtonNotification'></button>
+                <button onClick={handleOnClickNumberNotifications} className='ButtonBottom' id='ButtonNumberNotification'></button>
+                <button onClick={handleOnClickProfile} className='ButtonBottom' id='ButtonProfile'></button>
+                <button onClick={handleOnClickMenu} className='ButtonBottom' id='ButtonMenu'></button>
+            </div>
+        </HeaderContainer>
+    )
+}
+
+export default MasterHeader;
