d46ff2e86a51b3e797790e19332db24fc9579e89
mijaplex <mijail.febres@gmail.com>
2021-09-14 11:01:06 +0200

    Thumbnails added, some functionality is missing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Login from './Pages/Login/Login';
 import QuestionContainer from "./Pages/Login/QuestionTop";
+import PublishContainer from './Pages/Publish/PublishSomething';
 
 function App() {
   return (
     <div className="App">
+      <PublishContainer/>
       <QuestionContainer label = {'Don\'t have an account'} value='SIGN UP'/>
       <Login />
     </div>
