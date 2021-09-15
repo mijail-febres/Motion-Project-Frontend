0aa804248e8af625026da55535606ebf877bdc21
Patrick <patrickanddaniel100@gmail.com>
2021-09-15 15:53:00 +0200

    Finished all Post variations & Posts page
    Signed-off-by: Patrick

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import CopyrightContainer from './Pages/Login/Copyright';
 import MasterHeader from './Components/Posts/Header/Header';
 import Login from './Pages/Login/Login';
 import { GlobalStyle } from './Styles';
-import QuestionContainer from "./Pages/Login/QuestionTop";
+import QuestionContainer from "./Components/Login/QuestionTop/QuestionTop";
 import Posts from './Pages/Posts/Posts';
 import PublishContainer from '../src/Components/PublishSomething/PublishSomething';
 
@@ -11,12 +11,13 @@ const nNotif = 10; // just to test how notifications are shown. This works nicel
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
+      {/* <GlobalStyle />
       <PublishContainer/>
       <MasterHeader NumberOfNotifications = {nNotif}/>
       <CopyrightContainer label ={'Motion 2018. All rights reserved'}/>
       <QuestionContainer label = {'Don\'t have an account'} value='SIGN UP'/>
-      <Login />
+      <Login /> */}
+      <Posts />
     </div>
   );
 }
