889035202d1c5a265fe8d71035e0f4a2f6da0041
Adrien <adrien.laaboudi@gmail.com>
2021-09-16 16:41:05 +0200

    Authentication and Login processes working in Console

--- src/Components/Login/SignUpForm/SignUpForm.jsx
+++ src/Components/Login/SignUpForm/SignUpForm.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useHistory } from 'react-router';
+import { useState } from 'react';
 import SignUpVerifyPage from '../../../Pages/Signup/SignUpVerify/SignUpVerifyPage';
+import registrationAction from '../../../Store/Actions/registrationAction';
 
 function SignUpForm() {
-    const history = useHistory();
-    const routeChange = () => {
-        let path = "./signupverify";
-        history.push(path)
+    const [email, setEmail] = useState('');
+
+    const submitSignUp = async (e) => {
+        e.preventDefault()
+        registrationAction(email)
     }
+    
     return (
         <div>
             <div className='instruction' >
@@ -15,11 +19,11 @@ function SignUpForm() {
             </div>
 
             <div className='inputWrapper' >
-                <input type='text' placeholder='Email' ></input>
+                <input type='text' placeholder='Email' onChange={(e) => setEmail(e.target.value)} value={email}></input>
             </div>
 
             <div className='buttonWrapper' >
-                <button onClick={routeChange}>Continue</button>
+                <button onClick={submitSignUp}>Continue</button>
             </div>
 
 
--- src/Store/Actions/registrationAction.jsx
+++ src/Store/Actions/registrationAction.jsx
@@ -1,16 +1,28 @@
 import { baseUrl } from "./loginAction";
+import { userLogin } from "./loginActionbak.bak";
+import { USER_REGISTRATION } from "./actionTypes";
 
-const registrationAction = email => async (dispatch, getState) => {
+const userRegistrationAction = email => ({
+    type: USER_REGISTRATION,
+    payload: email
+})
+
+const registrationAction = async email => {
+    console.log('email')
     const headers = new Headers({
         'Content-Type': 'application/json'
     });
-    const body = JSON.stringify(email);
-    const config = {
-        headers,
-        body,
-        method: 'POST'
+    const body = {
+        "email": email,
     };
-    const response = await fetch(`${baseUrl}/auth/registration/`, config);
+
+    const method = 'POST'
+    const config = {
+        method,
+        headers,
+        body: JSON.stringify(body)
+    };
+    const response = await fetch(`${baseUrl}/backend/api/auth/registration/`, config);
     if(response.status === 200) return null;
     const data = await response.json();
     const keys = Object.keys(data)
